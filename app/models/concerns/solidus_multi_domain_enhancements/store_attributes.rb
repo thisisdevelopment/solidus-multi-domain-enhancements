a511fb362779b596211744c82b84cf00c74219c3
Seb Ashton <seb@sebashton.com>
2016-05-31 16:17:39 +0100

    Prefer to use symbols for locales
    feels better

--- app/models/concerns/solidus_multi_domain_enhancements/store_attributes.rb
+++ app/models/concerns/solidus_multi_domain_enhancements/store_attributes.rb
@@ -4,10 +4,11 @@ module SolidusMultiDomainEnhancements
 
     included do
       validates :order_number_prefix, uniqueness: true
+      validates :default_locale, format: { without: /\s/ }
     end
 
     def default_locale
-      self[:default_locale] || I18n.default_locale.to_s
+      self[:default_locale].try(:to_sym) || I18n.default_locale
     end
   end
 end
--- spec/unit/store/default_locale_spec.rb
+++ spec/unit/store/default_locale_spec.rb
@@ -4,12 +4,12 @@ describe Spree::Store do
   subject { store.default_locale }
 
   context 'when no locale is set' do
-    it { is_expected.to eq('en') }
+    it { is_expected.to eq(:en) }
   end
 
   context 'when the locale is set to fr' do
     let(:store) { create(:store, :with_default_locale) }
 
-    it { is_expected.to eq('fr') }
+    it { is_expected.to eq(:fr) }
   end
 end
