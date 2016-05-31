9c7d924b3d3f11c45979b07fa2939c357c81ba70
Rory MacDonald <rory@madebymade.co.uk>
2016-05-31 18:41:59 +0100

    Include language in orders API

---
+++ spec/requests/order_language_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'language attribute within the orders API' do
+  let(:store) { create(:store) }
+  let(:user) { create(:user).tap(&:generate_spree_api_key!) }
+  let(:order) { create(:order_with_line_items, user: user) }
+
+  it 'uses the stores default_locale' do
+    get '/api/orders/#{order.number}', token: user.spree_api_key
+
+    order_response = JSON.parse(response.body)
+    expect(order_response[:language]).to eq(store.default_locale)
+  end
+end
