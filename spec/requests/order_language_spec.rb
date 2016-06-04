90a10a49971e712499227c42788e28e8d4c014a5
Seb Ashton <seb@sebashton.com>
2016-06-04 21:24:38 +0100

    Fix build.
    String interpolation requires double quotes

--- spec/requests/order_language_spec.rb
+++ spec/requests/order_language_spec.rb
@@ -6,7 +6,7 @@ describe 'language attribute within the orders API' do
   let(:order) { create(:order_with_line_items, user: user) }
 
   it 'uses the stores default_locale' do
-    get '/api/orders/#{order.number}', token: user.spree_api_key
+    get "/api/orders/#{order.number}", token: user.spree_api_key
 
     order_response = JSON.parse(response.body)
     expect(order_response[:language]).to eq(store.default_locale)
