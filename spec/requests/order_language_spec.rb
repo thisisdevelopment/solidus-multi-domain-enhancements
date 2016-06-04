64ab8d2e336d5a75ade33781b27e78ee21b6e04f
Seb Ashton <seb@sebashton.com>
2016-06-04 22:07:42 +0100

    Fix request test

--- spec/requests/order_language_spec.rb
+++ spec/requests/order_language_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-xdescribe 'language attribute within the orders API' do
+describe 'language attribute within the orders API' do
   let(:store) { create(:store) }
   let(:user) { create(:user).tap(&:generate_spree_api_key!) }
-  let(:order) { create(:order_with_line_items, user: user) }
+  let(:order) { create(:order_with_line_items, user: user, store: store) }
 
   it 'uses the stores default_locale' do
     get "/api/orders/#{order.number}", token: user.spree_api_key
 
     order_response = JSON.parse(response.body)
-    expect(order_response[:language]).to eq(store.default_locale)
+    expect(order_response['language']).to eq(store.default_locale.to_s)
   end
 end
