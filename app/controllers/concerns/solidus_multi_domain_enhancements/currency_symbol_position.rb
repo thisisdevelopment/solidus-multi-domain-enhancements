d5b6e2be1310faba939f323ea70ea8439c128e19
Seb Ashton <seb@sebashton.com>
2016-06-23 13:57:29 +0100

    Symbol position is what I really wanted

--- app/controllers/concerns/solidus_multi_domain_enhancements/currency_symbol_position.rb
+++ app/controllers/concerns/solidus_multi_domain_enhancements/currency_symbol_position.rb
@@ -9,9 +9,15 @@ module SolidusMultiDomainEnhancements
     private
 
     def set_currency_symbol_position
-      Spree::Money.default_formatting_rules.merge!(
-        sign_before_symbol: current_store.currency_symbol_first
-      )
+      Spree::Money.default_formatting_rules.merge!(symbol_position)
+    end
+
+    def symbol_position
+      if current_store.currency_symbol_before
+        { symbol_position: :before }
+      else
+        { symbol_position: :after }
+      end
     end
   end
 end
