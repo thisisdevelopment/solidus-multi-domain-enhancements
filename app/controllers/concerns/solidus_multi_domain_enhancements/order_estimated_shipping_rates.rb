c0c3ce107026fccbdef3238df162bad4782484fa
Kyle <kjdchapman@gmail.com>
2018-05-08 12:39:46 +0100

    Only use enabled shipping methods to calc cost

--- app/controllers/concerns/solidus_multi_domain_enhancements/order_estimated_shipping_rates.rb
+++ app/controllers/concerns/solidus_multi_domain_enhancements/order_estimated_shipping_rates.rb
@@ -5,7 +5,14 @@ module SolidusMultiDomainEnhancements
     def estimate_shipping_rates
       authorize! :show, @order, order_token
 
-      @estimate_package = SolidusMultiDomainEnhancements::EstimateShippingRates.new(@order, current_store.shipping_methods).for_rabl
+      available_shipping_methods = current_store
+        .shipping_methods
+        .where
+        .not(display_on: 'back_end')
+
+      @estimate_package = SolidusMultiDomainEnhancements::EstimateShippingRates
+        .new(@order, available_shipping_methods)
+        .for_rabl
 
       respond_with(@estimate_package)
     end
